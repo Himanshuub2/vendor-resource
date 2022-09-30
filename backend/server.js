9b6e5388aff0ea3242796b493a1dcca11ab5f43f
himanshu Khemot <2121997him@gmail.com>
2022-09-30 19:08:30 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -5,13 +5,18 @@ import cors from "cors"
 import bodyParser from "body-parser";
 import Routes from "./Routes.js";
 import multer from "multer";
+import path from "path"
+import {fileURLToPath} from "url"
+// import { dirname } from "path";
 
 
-const port = 4000;
+const port = process.env.PORT || 4000;
 const app = express();
 app.use(cors())
 app.use(bodyParser.json({extended:true}))
 app.use(bodyParser.urlencoded({extended:true}))
+
+
 
 
 
@@ -21,12 +26,32 @@ const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
 Connection(username,password);
 
+
+
+
 ///server setting
 app.listen(port,()=>{
     console.log("hello All") 
 })
 
+
 app.use("/",Routes)
+
+////deployment///
+const __dirname = path.resolve();
+if(process.env.NODE_ENV ==="production"){
+
+    app.use(express.static(path.join(__dirname,"../client/build")));
+
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,"../","client","build","index.html"))
+    })
+
+}else{
+    app.listen(port,()=>{
+        console.log("hello All") 
+    })
+}
 
 
 //multer storage
